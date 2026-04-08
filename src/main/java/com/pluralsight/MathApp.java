c09f6f32b85e309a8c8c7b1390a3064c7af4f2ec
WendolynQuartey <quarteywendolyn@gmail.com>
2026-04-08 08:50:38 -0400

    finished question 6

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -3,17 +3,29 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
         // Question 1:
-        // declare variables here
-        // then code solution
-        // then use System.out.println() to display results
-        // ex: System.out.println("The answer is " + answer);
-        // REPEAT FOR NEXT EXERCISE
-
-        int bobSalary, garySalary;
-        int highestSalary;
-        bobSalary = 120000;
-        garySalary = 110000;
-        highestSalary = Math.max(garySalary, bobSalary);
+        int bobSalary = 120000;
+        int garySalary = 110000;
+        int highestSalary = Math.max(garySalary, bobSalary);
         System.out.println("The highest salary is " + highestSalary);
+
+        // Question 2:
+        double carPrice = 20000.99;
+        double truckPrice = 2250.83;
+        System.out.println(Math.min(carPrice, truckPrice));
+
+        // Question 3:
+        double radius = 7.25;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("Thea area of the circle with a radius of " + radius + " is " + area);
+
+        // Question 4:
+        double num = 5.0;
+        System.out.println(Math.sqrt(num));
+
+        // Question 5:
+
+        // Question 6:
+        double numVal = -3.8;
+        System.out.println("The absolute value of this number is " + Math.abs(numVal));
     }
 }
